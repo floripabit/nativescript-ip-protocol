0b7f4e2c10f93b386463223924fd6b0c09deaf85
AdrianoOP <oliveira.pires@gmail.com>
2020-04-23 20:40:25 -0300

    Merged stash

--- src/udp/udp.ios.d.ts
+++ src/udp/udp.ios.d.ts
@@ -1,8 +1,25 @@
 import { Observable } from 'rxjs';
+<<<<<<< Updated upstream
 import { UdpCommon } from './udp.common';
 export declare class UdpProtocol extends UdpCommon {
+=======
+export declare class UdpProtocol extends NSObject implements GCDAsyncUdpSocketDelegate {
+    static ObjCProtocols: any;
+    private udpServerSubject;
+    private udpClientSubject;
+    private tag;
+>>>>>>> Stashed changes
     constructor();
     receive(port: number): Observable<string>;
     sendUnicast(address: string, port: number, msg: string): Observable<string>;
     sendBroadcast(port: number, msg: string): Observable<string>;
+<<<<<<< Updated upstream
+=======
+    udpSocketDidCloseWithError(sock: GCDAsyncUdpSocket, error: NSError): void;
+    udpSocketDidConnectToAddress(sock: GCDAsyncUdpSocket, address: NSData): void;
+    udpSocketDidNotConnect(sock: GCDAsyncUdpSocket, error: NSError): void;
+    udpSocketDidNotSendDataWithTagDueToError(sock: GCDAsyncUdpSocket, tag: number, error: NSError): void;
+    udpSocketDidReceiveDataFromAddressWithFilterContext(sock: GCDAsyncUdpSocket, data: NSData, address: NSData, filterContext: any): void;
+    udpSocketDidSendDataWithTag(sock: GCDAsyncUdpSocket, tag: number): void;
+>>>>>>> Stashed changes
 }
